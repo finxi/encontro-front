402e27e36118e45d658047029f60e26d5dd96e58
Luccas <luccasmaia@poli.ufrj.br>
2021-07-27 17:48:36 -0300

    correction according to eslint

--- form-test/src/router/index.ts
+++ form-test/src/router/index.ts
@@ -1,18 +1,17 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "Home",
-    component: ()=> import('@/views/Home.vue'),
-  }
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home.vue'),
+  },
 ];
-
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
 });
--- form-test/src/views/Home.vue
+++ form-test/src/views/Home.vue
@@ -3,7 +3,7 @@
     <h1>Olá</h1>
   </div>
 </template>
-
 <script lang="ts">
-
+import { defineComponent } from '@vue/composition-api';
+export default defineComponent({});
 </script>
