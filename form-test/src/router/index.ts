dc4eeca2f084f9c0ac4c773f15269f91a31244aa
Luccas <luccasmaia@poli.ufrj.br>
2021-07-27 17:08:59 -0300

    clear project

--- form-test/src/router/index.ts
+++ form-test/src/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,17 +7,8 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    component: ()=> import('@/views/Home.vue'),
+  }
 ];
 
 const router = new VueRouter({
--- form-test/src/views/Home.vue
+++ form-test/src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Olá</h1>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-export default Vue.extend({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-});
 </script>
