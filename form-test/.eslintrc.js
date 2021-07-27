0e97cf5f91ea4c6df13e95cdb19b13cb9da2cb69
Luccas <luccasmaia@poli.ufrj.br>
2021-07-27 17:47:38 -0300

    fix config eslint

--- form-test/.eslintrc.js
+++ form-test/.eslintrc.js
@@ -1,70 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:vue/recommended',
     'plugin:vue/essential',
-    'plugin:vue/strongly-recommended',
-    'plugin:vue/base',
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
   ],
-  plugins: [
-    'import',
-    'sort-imports-es6-autofix',
-  ],
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'padding-line-between-statements': [
-      'error',
-      {
-        blankLine: 'always',
-        prev: ['const', 'let', 'var', 'import', 'export', 'default', 'directive'],
-        next: '*',
-      },
-      {
-        blankLine: 'always',
-        prev: '*',
-        next: ['return', 'var', 'let', 'const', 'block', 'block-like', 'throw'],
-      },
-      { blankLine: 'never', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
-      { blankLine: 'never', prev: 'import', next: 'import' },
-      { blankLine: 'any', prev: 'export', next: 'export' },
-    ],
-    'prefer-destructuring': 0,
-    'sort-imports-es6-autofix/sort-imports-es6': 2,
-    'import/extensions': 0,
-    'import/no-extraneous-dependencies': 0,
-    'import/no-unresolved': 0,
-    'import/prefer-default-export': 0,
-    'max-len': [
-      'error',
-      {
-        code: 120,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreComments: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-        ignorePattern: 'trans\\(|transChoice\\(',
-      },
-    ],
-    'no-restricted-imports': [
-      'error',
-      {
-        paths: ['lodash', 'moment'],
-        patterns: ['lodash/*', './*', '../*'],
-      },
-    ],
   },
 };
