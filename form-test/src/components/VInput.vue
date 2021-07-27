d0f678e364d48118d375c069a674f4b0b53c46ca
Luccas <luccasmaia@poli.ufrj.br>
2021-07-27 20:29:41 -0300

    wip v-input

---
+++ form-test/src/components/VInput.vue
@@ -0,0 +1,15 @@
+<template>
+<input type="text">
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+
+})
+</script>
+
+<style>
+
+</style>
