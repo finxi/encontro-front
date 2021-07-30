02dfb18ba213eb37e52af608c81f598edf392887
Luccas <luccasmaia@poli.ufrj.br>
2021-07-30 16:46:00 -0300

    add eslint rules

--- form-test/.eslintrc.js
+++ form-test/.eslintrc.js
@@ -18,5 +18,38 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'var', 'import', 'export', 'default', 'directive'],
+        next: '*',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['return', 'var', 'let', 'const', 'block', 'block-like', 'throw'],
+      },
+      { blankLine: 'never', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      { blankLine: 'never', prev: 'import', next: 'import' },
+      { blankLine: 'any', prev: 'export', next: 'export' },
+    ],
+    'prefer-destructuring': 0,
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreComments: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+        ignorePattern: 'trans\\(|transChoice\\(',
+      },
+    ],
   },
 };
