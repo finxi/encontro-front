aa117fac2d21462be5a25e37aac886d31d5ba31b
Luccas <luccasmaia@poli.ufrj.br>
2021-07-30 16:48:42 -0300

    add mdi and composition api

--- form-test/src/main.ts
+++ form-test/src/main.ts
@@ -1,11 +1,12 @@
-import '@/composition-api'
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import '@/composition-api';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
