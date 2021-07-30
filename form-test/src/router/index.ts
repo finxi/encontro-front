3ca1f401cf16ac88a4592b7b3f67038e1b7a9b8d
Luccas <luccasmaia@poli.ufrj.br>
2021-07-30 16:49:37 -0300

    dynamically route title renaming

--- form-test/src/router/index.ts
+++ form-test/src/router/index.ts
@@ -8,12 +8,20 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: () => import('@/views/Home.vue'),
+    meta: { title: 'Início' },
   },
 ];
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+const DEFAULT_TITLE = 'Formulário';
+
+router.afterEach((to, _from) => {
+  Vue.nextTick(() => {
+    document.title = to.meta?.title || DEFAULT_TITLE;
+  });
 });
 
 export default router;
